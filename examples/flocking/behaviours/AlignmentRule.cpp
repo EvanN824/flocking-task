ab94592a512f514fce1723bd4ffdc80826284b1c
EvanN824 <70778356+EvanN824@users.noreply.github.com>
2022-09-11 16:30:44 -0400

    Alignmenttest

--- examples/flocking/behaviours/AlignmentRule.cpp
+++ examples/flocking/behaviours/AlignmentRule.cpp
@@ -4,8 +4,17 @@
 Vector2 AlignmentRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
     Vector2 averageVelocity = Vector2::zero();
 
+    int numBoids = neighborhood.size();
+
     // todo: add your code here to align each boid in a neighborhood
     // hint: iterate over the neighborhood
+
+    for (Boid* b : neighborhood)
+    {
+        averageVelocity += b->velocity;
+    }
+
+    averageVelocity /= numBoids;
 
     return Vector2::normalized(averageVelocity);
 }
